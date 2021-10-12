20a6c4e41e850a4c991d451ffde307914e3b0449
Mason Davenport <mad2026@utulsa.edu>
2021-10-12 13:48:01 -0500

    todo: finish building functions implement switch

--- MyCalculator/MyCalculator.js
+++ MyCalculator/MyCalculator.js
@@ -1,31 +1,65 @@
-var $ = function(id) {
+var $ = function (id) {
 	return document.getElementById(id);
 };
 
-var calculate = function() {
-	var subtotal = parseFloat($("subtotal").value);
-	var taxRate = parseFloat($("tax_rate").value);
-	var shipping = parseFloat($("shipping_charges").value);
-	var salesTax = $("sales_tax").value;
-	var total = $("total").value;
+var menu = function () {
+	var menuOption = "";
+	for (; ;) {
+		menuOption = parseInt(prompt("Menu\n\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exponent\n6. Mean\n7. Quit"));
+		if (!isNaN(menuOption) && menuOption > 0 && menuOption < 8) {
+			break
+		}
+		else {
+			alert("Please enter a number 1-7")
+		}
+	}
+}
+var addition = function () {
+}
+var subtraction = function () {
+}
+var multiplication = function () {
+}
+var division = function () {
+}
+var exponential = function () {
+}
+var average = function () {
+}
+var exitProgram = function () {
+}
 
-	salesTax = subtotal * (taxRate/100);
-	salesTax = parseFloat(salesTax.toFixed(2));
-	total = salesTax + subtotal + shipping;
-	
-	$("sales_tax").value = "$" + salesTax.toFixed(2);
-	$("total").value = "$" + total.toFixed(2);
-	return false;
-	
+///*
+switch (menuOption - 1) {
+	case 0://Add
+		addition();
+		break;
+	case 1://Subtract
+		subtraction();
+		break;
+	case 2://Multiply
+		multiplication();
+		break;
+	case 3://Divide
+		division();
+		break;
+	case 4://Exponent
+		exponential();
+		break;
+	case 5://Mean
+		average();
+		break;
+	case 6://Quit
+		exitProgram();
+}
+//*/
+menu();
 
-};
-
- window.onload = function () 
-{
-    $("subtotal").value = "";
-    $("tax_rate").value = "";
+window.onload = function () {
+	$("subtotal").value = "";
+	$("tax_rate").value = "";
 	$("shipping_charges").value = "";
-    $("calculate").onclick = calculate;
-    //$("monthly_payment").focus();
+	$("calculate").onclick = calculate;
+	//$("monthly_payment").focus();
 }
 
