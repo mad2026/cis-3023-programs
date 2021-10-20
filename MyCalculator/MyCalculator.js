b66dc906ef710f4d5f9c0d57e8eb72fde850ce5f
Mason Davenport <mad2026@utulsa.edu>
2021-10-19 23:17:11 -0500

    Finialized the MyCalculator program / assignment 2

--- MyCalculator/MyCalculator.js
+++ MyCalculator/MyCalculator.js
@@ -2,11 +2,12 @@ var $ = function (id) {
 	return document.getElementById(id);
 };
 //variables go here
-var menuOption = "";
+var menuOption;
 var num1;
 var num2;
-var meanCount;
-for (; ;) {
+var meanCount = 0;
+var breakValue = "";
+for (; breakValue !="EXIT";) {
 	var menu = function () {
 		for (; ;) {
 			menuOption = parseInt(prompt("Menu\n\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exponent\n6. Mean\n7. Quit"));
@@ -14,95 +15,115 @@ for (; ;) {
 				break
 			}
 			alert("Please enter a number 1-7");
-		}
-	}
+		}//for
+	}//menu function
+
 	var addition = function () {
+		num1 = 0;
+		num2 = 0;
 		for (; ;) {
 			num1 = parseFloat(prompt("Enter the first number here"));
 			if (!isNaN(num1)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		for (; ;) {
 			num2 = parseFloat(prompt("Enter the second number here"));
 			if (!isNaN(num2)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		alert(num1 + num2);
-	}
+	}//addition function
+
 	var subtraction = function () {
+		num1 = 0;
+		num2 = 0;
 		for (; ;) {
 			num1 = parseFloat(prompt("Enter the first number here"));
 			if (!isNaN(num1)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		for (; ;) {
 			num2 = parseFloat(prompt("Enter the second number here that will be subtracted from the first"));
 			if (!isNaN(num2)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		alert(num1 - num2);
-	}
+	}//subtraction function
+
 	var multiplication = function () {
+		num1 = 0;
+		num2 = 0;
 		for (; ;) {
 			num1 = parseFloat(prompt("Enter the first number here"));
 			if (!isNaN(num1)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		for (; ;) {
 			num2 = parseFloat(prompt("Enter the second number here that will be multiplied with the first"));
 			if (!isNaN(num2)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		alert(num1 * num2);
-	}
+	}//multiplication function
+
 	var division = function () {
+		num1 = 0;
+		num2 = 0;
 		for (; ;) {
 			num1 = parseFloat(prompt("Enter the first number / numerator here"));
 			if (!isNaN(num1)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		for (; ;) {
 			num2 = parseFloat(prompt("Enter the second number/ denominator here that the first number will be divided by"));
 			if (!isNaN(num2)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		alert(num1 / num2);
-	}
+	}//division function
+
 	var exponential = function () {
+		num1 = 0;
+		num2 = 0;
 		for (; ;) {
 			num1 = parseFloat(prompt("Enter the base number here"));
 			if (!isNaN(num1)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		for (; ;) {
 			num2 = parseFloat(prompt("Enter the exponent here"));
 			if (!isNaN(num2)) {
 				break
 			}
 			alert("Please enter a number");
-		}
+		}//for
 		alert(num1 ** num2);
-	}
+	}//exponential function
+
 	var average = function () {
+		num1 = 0;
+		num2 = 0;
+		meanCount = 0;
 		for (; ;) {
+			//alert(num2)
 			num1 = prompt("Enter a number to add to the average here or enter '***' to end");
 			if (!isNaN(parseFloat(num1))) {
 				num2 += parseFloat(num1);
@@ -114,11 +135,14 @@ for (; ;) {
 			else {
 				alert("Please enter a number");
 			}
-		}
-		alert(parseFloat(num2) / meanCount);
-	}
+		}//for
+		alert(num2 / meanCount);
+	}//average function
+	
 	var exitProgram = function () {
-	}
+		alert("Thank you for using my Calculator, have a nice day!");
+		breakValue = "EXIT";
+	}//exit function
 
 
 	///*
@@ -146,8 +170,9 @@ for (; ;) {
 			break;
 		case 6://Quit
 			exitProgram();
-	}
-}
+			break;
+	}//switch
+}//for
 window.onload = function () {
 	//$("subtotal").value = "";
 	//$("tax_rate").value = "";
