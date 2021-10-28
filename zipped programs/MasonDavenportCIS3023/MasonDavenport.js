dc08f9b44a0c8816820d4a0131d25ac15c65c076
Mason Davenport <mad2026@utulsa.edu>
2021-10-28 10:37:56 -0500

    changed stuff per requirements i guess, idrk

--- zipped programs/MasonDavenportCIS3023/MasonDavenport.js
+++ zipped programs/MasonDavenportCIS3023/MasonDavenport.js
@@ -1,18 +1,20 @@
 var $ = function (id) {
 	return document.getElementById(id);
 };
-for (; ;) {
-	vowelCount = 0;
-	var usrInput = prompt("Please enter a string here to count the total vowels or enter '***' to end");
-	if (usrInput == "***") {
-		alert("Thank you for using Vowel Counter, have a nice day!");
-		break
-	}//if
-	for (var i = 0; i < usrInput.length; i++) {
-		if (usrInput.charAt(i).toLowerCase() == "a" || usrInput.charAt(i).toLowerCase() == "e" || usrInput.charAt(i).toLowerCase() == "i" || usrInput.charAt(i).toLowerCase() == "o" || usrInput.charAt(i).toLowerCase() == "u") {
-			vowelCount++;
-		}
+var vowelCounter = function () {
+	for (; ;) {
+		vowelCount = 0;
+		var usrInput = prompt("Please enter a string here to count the total vowels or enter '***' to end");
+		if (usrInput == "***") {
+			alert("Thank you for using Vowel Counter, have a nice day!");
+			break
+		}//if
+		for (var i = 0; i < usrInput.length; i++) {
+			if (usrInput.charAt(i).toLowerCase() == "a" || usrInput.charAt(i).toLowerCase() == "e" || usrInput.charAt(i).toLowerCase() == "i" || usrInput.charAt(i).toLowerCase() == "o" || usrInput.charAt(i).toLowerCase() == "u") {
+				vowelCount++;
+			}
+		}//for
+		document.getElementById("myHeader").innerHTML = vowelCount;
 	}//for
-	alert(vowelCount);
-}//for
+}//vowel Function
 
