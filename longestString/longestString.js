d746277441ff4deb243ed2d3e6e017557140d297
Mason Davenport <mad2026@utulsa.edu>
2021-11-02 13:48:16 -0500

    commented code

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -5,26 +5,27 @@ for (; ;) {
 	var longestString;
 	var usrInput = "";
 	var alphaLower = "abcdefghijklmnopqrstuvwxyz"
+	var usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");
+	if (usrInput == "***") {
+		break//quit program
+	}//if
+	if (isNaN(longestString)) {//assign blank variable to a number value
+		longestString = getStrings(usrInput);
+	}//if
+	if (getStrings(usrInput) > longestString) {//only save the highest number of unique lowercase letters
+		longestString = getStrings(usrInput);
+	}//if
+	
 	var getStrings = function (usrInput) {
 		var uniqueCount = 0;
-		usrInput = usrInput.replace(/\s/g, '').toLowerCase();
-		for (var i = 0; i < alphaLower.length; i++) {
-			if (usrInput.includes(alphaLower.charAt(i))) {
+		usrInput = usrInput.replace(/\s/g, '').toLowerCase();//remove whitespace and change to lowercase
+		for (var i = 0; i < alphaLower.length; i++) {//index through the alphabet
+			if (usrInput.includes(alphaLower.charAt(i))) {//check if the user input contains a letter, if so add to the count
 				uniqueCount++;
-			}
+			}//if
 		}//for
 		return (uniqueCount);
 	}//getStrings function
-	
-	usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");
-	if (usrInput == "***") {
-		break
-	}//if
-	if (isNaN(longestString)) {
-		longestString = getStrings(usrInput);
-	}//if
-	if (getStrings(usrInput) > longestString) {
-		longestString = getStrings(usrInput);
-	}// if
+
 }//for
 alert(longestString);
