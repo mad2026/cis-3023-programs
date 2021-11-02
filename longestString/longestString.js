87fbf5282e355606d08b575da70d86ca060ed17f
Mason Davenport <mad2026@utulsa.edu>
2021-11-02 13:37:06 -0500

    can now pass the function a value

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -1,35 +1,30 @@
 var $ = function (id) {
 	return document.getElementById(id);
 };
-for (; usrInput != "***";) {
-	var longestString = 0;
+for (; ;) {
+	var longestString;
 	var usrInput = "";
 	var alphaLower = "abcdefghijklmnopqrstuvwxyz"
-	var getStrings = function () {
-		for (; ;) {
-			var uniqueCount = 0;
-			usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");
-			if (usrInput == "***") {
-				break
-			}//if
-			usrInput = usrInput.replace(/\s/g, '').toLowerCase();
-			for (var i = 0; i < alphaLower.length; i++) {
-				if (usrInput.includes(alphaLower.charAt(i))) {
-					uniqueCount++;
-				}
-				//alert(alphaLower.charAt(i));
-			}//for
-			//alert(usrInput);
-			//alert(uniqueCount);
-
-			if (uniqueCount > longestString) {
-				longestString = uniqueCount;
-			}// if
-
-
-			//alert(usrInput.length);
+	var getStrings = function (usrInput) {
+		var uniqueCount = 0;
+		usrInput = usrInput.replace(/\s/g, '').toLowerCase();
+		for (var i = 0; i < alphaLower.length; i++) {
+			if (usrInput.includes(alphaLower.charAt(i))) {
+				uniqueCount++;
+			}
 		}//for
-		alert(longestString);
+		return (uniqueCount);
 	}//getStrings function
-	getStrings()
+	
+	usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");
+	if (usrInput == "***") {
+		break
+	}//if
+	if (isNaN(longestString)) {
+		longestString = getStrings(usrInput);
+	}//if
+	if (getStrings(usrInput) > longestString) {
+		longestString = getStrings(usrInput);
+	}// if
 }//for
+alert(longestString);
