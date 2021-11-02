19935f3db075d691e288640e8606739f7c51cc3b
Mason Davenport <mad2026@utulsa.edu>
2021-11-02 13:20:15 -0500

    code works. still need to pass input to function

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,23 +2,34 @@ var $ = function (id) {
 	return document.getElementById(id);
 };
 for (; usrInput != "***";) {
-	var breakValue = "";
+	var longestString = 0;
 	var usrInput = "";
+	var alphaLower = "abcdefghijklmnopqrstuvwxyz"
 	var getStrings = function () {
-		var stringArray = [];
 		for (; ;) {
-			//alert(num2)
+			var uniqueCount = 0;
 			usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");
 			if (usrInput == "***") {
 				break
-			}
-			stringArray[stringArray.length] = usrInput;
-		}//for
-		alert(stringArray);
-	}//getStrings function
+			}//if
+			usrInput = usrInput.replace(/\s/g, '').toLowerCase();
+			for (var i = 0; i < alphaLower.length; i++) {
+				if (usrInput.includes(alphaLower.charAt(i))) {
+					uniqueCount++;
+				}
+				//alert(alphaLower.charAt(i));
+			}//for
+			//alert(usrInput);
+			//alert(uniqueCount);
 
-	var countAlpha = function(stringArray){
-		
-	}
+			if (uniqueCount > longestString) {
+				longestString = uniqueCount;
+			}// if
+
+
+			//alert(usrInput.length);
+		}//for
+		alert(longestString);
+	}//getStrings function
 	getStrings()
 }//for
