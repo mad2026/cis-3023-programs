12ed439583e039615d8be98c370daaef7588d727
Mason Davenport <mad2026@utulsa.edu>
2021-11-04 13:02:59 -0500

    i think its done?

--- MasonDavenportCIS3023Midterm/MasonDavenport.js
+++ MasonDavenportCIS3023Midterm/MasonDavenport.js
@@ -1,31 +1,22 @@
 var $ = function (id) {
 	return document.getElementById(id);
 };
-for (; ;) {
-	var longestString;
-	var usrInput = "";
-	var alphaLower = "abcdefghijklmnopqrstuvwxyz"
-	var usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");
-	if (usrInput == "***") {
-		break//quit program
-	}//if
-	if (isNaN(longestString)) {//assign blank variable to a number value
-		longestString = getStrings(usrInput);
-	}//if
-	if (getStrings(usrInput) > longestString) {//only save the highest number of unique lowercase letters
-		longestString = getStrings(usrInput);
-	}//if
-	
-	var getStrings = function (usrInput) {
-		var uniqueCount = 0;
-		usrInput = usrInput.replace(/\s/g, '').toLowerCase();//remove whitespace and change to lowercase
-		for (var i = 0; i < alphaLower.length; i++) {//index through the alphabet
-			if (usrInput.includes(alphaLower.charAt(i))) {//check if the user input contains a letter, if so add to the count
-				uniqueCount++;
-			}//if
+function insertUnderscore() {
+	for (; ;) {
+		var modifiedString = "";
+		var usrInput = "";
+		var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		var usrInput = prompt("Enter a String to insert underscores before ever uppercase letter or enter '***' to end");
+		if (usrInput == "***") {
+			break//quit program
+		}
+		modifiedString = usrInput;
+		for (var i = 0; i < alphaUpper.length; i++) {//index through the alphabet
+			modifiedString = modifiedString.replace(alphaUpper.charAt(i), "_" + alphaUpper.charAt(i));
 		}//for
-		return (uniqueCount);
-	}//getStrings function
-
-}//for
-alert(longestString);
+		return (modifiedString);
+	}//for
+}//insert underscore function
+function buttonFunction() {
+	alert(insertUnderscore());
+}//button function function
