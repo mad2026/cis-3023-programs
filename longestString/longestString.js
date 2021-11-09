4cd9b31be46fd10f169cf6a6936d243b5c13d258
Mason Davenport <mad2026@utulsa.edu>
2021-11-09 13:03:19 -0600

    refactored code. created main function

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -1,31 +1,30 @@
 var $ = function (id) {
 	return document.getElementById(id);
 };
-for (; ;) {
-	var longestString;
+var main = function () {
+	var longestString = "";
 	var usrInput = "";
+	for (; ;) {
+		var usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");
+		if (usrInput == "***") {
+			break//quit program
+		}//if
+		if (getStrings(usrInput) > getStrings(longestString)) {//only save the highest number of unique lowercase letters
+			longestString = usrInput;
+		}//if
+	}//for
+	return(longestString);
+}
+var getStrings = function (usrInput) {
 	var alphaLower = "abcdefghijklmnopqrstuvwxyz"
-	var usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");
-	if (usrInput == "***") {
-		break//quit program
-	}//if
-	if (isNaN(longestString)) {//assign blank variable to a number value
-		longestString = getStrings(usrInput);
-	}//if
-	if (getStrings(usrInput) > longestString) {//only save the highest number of unique lowercase letters
-		longestString = getStrings(usrInput);
-	}//if
-	
-	var getStrings = function (usrInput) {
-		var uniqueCount = 0;
-		usrInput = usrInput.replace(/\s/g, '').toLowerCase();//remove whitespace and change to lowercase
-		for (var i = 0; i < alphaLower.length; i++) {//index through the alphabet
-			if (usrInput.includes(alphaLower.charAt(i))) {//check if the user input contains a letter, if so add to the count
-				uniqueCount++;
-			}//if
-		}//for
-		return (uniqueCount);
-	}//getStrings function
+	var uniqueCount = 0;
+	usrInput = usrInput.replace(/\s/g, '').toLowerCase();//remove whitespace and change to lowercase
+	for (var i = 0; i < alphaLower.length; i++) {//index through the alphabet
+		if (usrInput.includes(alphaLower.charAt(i))) {//check if the user input contains a letter, if so add to the count
+			uniqueCount++;
+		}//if
+	}//for
+	return (uniqueCount);
+}//getStrings function
 
-}//for
-alert(longestString);
+alert("the string with the most characters was: " + main());
