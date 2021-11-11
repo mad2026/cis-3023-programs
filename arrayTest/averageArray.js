155b785be8ed7df40a7990807b3bd541c1f45fb9
Mason Davenport <mad2026@utulsa.edu>
2021-11-11 13:16:18 -0600

    created random array generator

--- arrayTest/averageArray.js
+++ arrayTest/averageArray.js
@@ -2,32 +2,36 @@ var $ = function (id) {
 	return document.getElementById(id);
 };
 var main = function () {
+	/*
 	var usrArray = [];
 	usrArray[0] = 141.95;
 	usrArray[1] = 212.25;
 	usrArray[2] = 411;
 	usrArray[3] = 135.75;
-	/*for (; ;) {
-		var usrInput = prompt("Enter a String to count the unique letters of here or enter '***' to end");//get user input
-		if (usrInput == "***") {
-			break//quit program
-		}//if
-	}//for*/
-	arrayAverage(usrArray);
-	//alert("the string with the most characters was: \"" + longestString + "\". With a unique count of \"" + getStrings(longestString) + "\".");
+	//*/
+	alert(arrayAverage(randomArrayGenerator()));
 }//main function
 var arrayAverage = function (usrArray) {
 	var sum = 0;
-	if(Array.isArray(usrArray)){
+	if (Array.isArray(usrArray)) {
 		for (var i = 0; i < usrArray.length; i++) {//index through the array
-			if(isNaN(parseFloat(usrArray[i]))){
-				return(false);
+			if (isNaN(parseFloat(usrArray[i]))) {
+				return (false);
 			}//if
-			sum +=usrArray[i]
+			sum += usrArray[i];
 		}//for
 	}//if
-	alert (sum/usrArray.length);
+	return (sum / usrArray.length);
 }//arrayAverage function
+
+var randomArrayGenerator = function () {
+var arraySize = Math.floor(Math.random() * 50) + 1;
+var usrArray = new Array(arraySize);
+for (var i = 0; i < usrArray.length; i++) {//index through the array
+	usrArray[i] = Math.floor(Math.random() * 100) + 1;
+}//for
+return(usrArray);
+}//randomArrayGenerator function
 
 
 main();
