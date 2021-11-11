8530d757b7ee90928011a5e45fb7daba1b1ad5ca
Mason Davenport <mad2026@utulsa.edu>
2021-11-11 13:48:12 -0600

    I think I finished it?

--- birthdayTwinChecker/birthdayTwinChecker.js
+++ birthdayTwinChecker/birthdayTwinChecker.js
@@ -2,37 +2,27 @@ var $ = function (id) {
 	return document.getElementById(id);
 };
 var main = function () {
-	/*
-	var usrArray = [];
-	usrArray[0] = 141.95;
-	usrArray[1] = 212.25;
-	usrArray[2] = 411;
-	usrArray[3] = 135.75;
-	//*/
-	alert(arrayAverage(randomArrayGenerator()));
+	alert(arrayMatcher(randomArrayGenerator()));
 }//main function
-var arrayAverage = function (usrArray) {
-	if (usrArray.length == 0) {
-		return (false);
-	}//if
-	var sum = 0;
-	if (Array.isArray(usrArray)) {
-		for (var i = 0; i < usrArray.length; i++) {//index through the array
-			if (isNaN(parseFloat(usrArray[i]))) {
-				return (false);
-			}//if
-			sum += usrArray[i];
-		}//for
-	}//if
-	return ((sum / usrArray.length).toFixed(2));
+var arrayMatcher = function (usrArray) {
+	var map = {};
+	var matchFound = false;
+	for (var i = 0; i < usrArray.length; i++) {//index through the array
+		if (map[usrArray[i]]) {
+			matchFound = true;
+			break;
+		}//if
+		map[usrArray[i]] = true;
+	}//for
+	return (matchFound);
 }//arrayAverage function
 
 var randomArrayGenerator = function () {
-	var arraySize = Math.floor(Math.random() * 50) + 1;
+	var arraySize = 25;
 	alert(arraySize);
 	var usrArray = new Array(arraySize);
 	for (var i = 0; i < usrArray.length; i++) {//index through the array
-		usrArray[i] = Math.floor(Math.random() * 100) + 1;
+		usrArray[i] = Math.floor(Math.random() * 365) + 1;
 	}//for
 	alert(usrArray);
 	return (usrArray);
