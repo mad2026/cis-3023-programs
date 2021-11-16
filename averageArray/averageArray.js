20b4cb88761d6a5952c6c72e1924a2dde6a6c3f7
Mason Davenport <mad2026@utulsa.edu>
2021-11-16 12:43:27 -0600

    adding comments

--- averageArray/averageArray.js
+++ averageArray/averageArray.js
@@ -12,27 +12,27 @@ var main = function () {
 	alert(arrayAverage(randomArrayGenerator()));
 }//main function
 var arrayAverage = function (usrArray) {
-	if (usrArray.length == 0) {
+	if (usrArray.length == 0) {//checks to make sure the size of the array is not zero
 		return (false);
 	}//if
 	var sum = 0;
 	if (Array.isArray(usrArray)) {
 		for (var i = 0; i < usrArray.length; i++) {//index through the array
-			if (isNaN(parseFloat(usrArray[i]))) {
+			if (isNaN(parseFloat(usrArray[i]))) {//check to see if value is not a number, if so return false.
 				return (false);
 			}//if
 			sum += usrArray[i];
 		}//for
 	}//if
-	return ((sum / usrArray.length).toFixed(2));
+	return ((sum / usrArray.length).toFixed(2));//return the average of the array
 }//arrayAverage function
 
 var randomArrayGenerator = function () {
-	var arraySize = Math.floor(Math.random() * 50) + 1;
+	var arraySize = Math.floor(Math.random() * 50) + 1;//create a random number for the array size between 1 and 50
 	alert(arraySize);
 	var usrArray = new Array(arraySize);
 	for (var i = 0; i < usrArray.length; i++) {//index through the array
-		usrArray[i] = Math.floor(Math.random() * 100) + 1;
+		usrArray[i] = Math.floor(Math.random() * 100) + 1;//procedurally create random numbers for each index of the array between 1 and 100
 	}//for
 	alert(usrArray);
 	return (usrArray);
