33e42fda0add74cf313e1d352344d6f394211447
Mason Davenport <mad2026@utulsa.edu>
2021-11-18 13:36:14 -0600

    just kidding now its working

--- coinTossRatio/coinTossRatio.js
+++ coinTossRatio/coinTossRatio.js
@@ -2,19 +2,13 @@ var $ = function (id) {
 	return document.getElementById(id);
 };
 var main = function () {
-	/*
-	var usrArray = [];
-	usrArray[0] = 141.95;
-	usrArray[1] = 212.25;
-	usrArray[2] = 411;
-	usrArray[3] = 135.75;
-	//*/
 	//head = 1
 	//tail = 2
 	var numSuccesses = 0;
 	var numTrials = parseInt(prompt("Enter the number of trials to run here"));
 	for (var i = 0; i < numTrials; i++) {//run the test the number of times the user requests
-		if (coinIsHead(randomNumGenerator())) {
+		//if (coinIsHead(randomNumGenerator())) {
+		if (getAtLeast5Heads10Tosses()) {
 			numSuccesses++;
 		}//if
 	}//for
@@ -29,10 +23,7 @@ var getAtLeast5Heads10Tosses = function () {
 			numSuccesses++;
 		}//if
 	}//for
-	if (numSuccesses >= 5) {
-		return ("Success");
-	}//if
-	return ("Failure");
+	return(numSuccesses >= 5);
 }//getAtLeast5Heads10Tosses function
 
 var coinIsHead = function (randNum) {
@@ -44,7 +35,7 @@ var coinIsHead = function (randNum) {
 
 var randomNumGenerator = function () {
 	var randNum = Math.floor(Math.random() * 2) + 1;//create a number 1 or 2
-	alert(randNum);
+	//alert(randNum);
 	return (randNum);
 }//randomNumGenerator function
 
